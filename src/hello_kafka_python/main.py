0dea3088a9bc7881aa4e5ca43ff7d663453ab225
marcello romani <marcello.romani.enit@gmail.com>
2025-09-07 01:32:10 +0100

    replace duplicated string with constant

--- src/hello_kafka_python/main.py
+++ src/hello_kafka_python/main.py
@@ -13,13 +13,15 @@ from kafka_commit_consumer import KafkaCommitConsumer
 from kafka_loop_producer import KafkaLoopProducer
 from msg_processors import PersistToTextFileMsgProcessor
 
+GROUP_ID: str = "group.id"
+
 producer_conf = {
     'bootstrap.servers': 'localhost:9092',
 }
 
 consumer_conf = {
     'bootstrap.servers': 'localhost:9092',
-    'group.id': 'REPLACE_ME',
+    GROUP_ID: 'REPLACE_ME',
     'auto.offset.reset': 'earliest',
 }
 
@@ -69,14 +71,14 @@ def main(consumer_type: str, consumer_group: str, producer_type: str, topic_name
 
     if consumer_type == 'basic':
         logger.info(f"Starting consumer type={consumer_type} topic={topic_name}, consumer_group={consumer_group}")
-        consumer_conf['group.id'] = consumer_group
+        consumer_conf[GROUP_ID] = consumer_group
         c = KafkaBasicConsumer(consumer_conf, topic_name, message_processor)
         register_client(c)
         c.run()
 
     elif consumer_type == 'commit':
         logger.info(f"Starting consumer type={consumer_type} topic={topic_name}, consumer_group={consumer_group}")
-        consumer_conf['group.id'] = consumer_group
+        consumer_conf[GROUP_ID] = consumer_group
         c = KafkaCommitConsumer(consumer_conf, topic_name, batch_size, message_processor)
         register_client(c)
         c.run()
