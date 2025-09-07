ea343dc0dbed6e57368fe752adabe9bdf92bc690
marcello romani <marcello.romani.enit@gmail.com>
2025-09-07 01:15:11 +0100

    use pathlib to read config

--- src/hello_kafka_python/logging_setup.py
+++ src/hello_kafka_python/logging_setup.py
@@ -1,8 +1,8 @@
 import json
 import logging.config
+from pathlib import Path
 
 
-def configure(logging_config_path: str):
-    with open(logging_config_path, "r") as fd:
-        conf = json.loads(fd.read())
+def configure(logging_config_path: Path):
+    conf = json.loads(logging_config_path.read_text())
     logging.config.dictConfig(config=conf)
